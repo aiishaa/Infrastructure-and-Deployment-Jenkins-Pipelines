b0cd3b932ac1c70506ba63735f9f3ecf00b09594
‪Aisha Fathy‬‏ <57088227+aiishaa@users.noreply.github.com>
2024-05-08 16:01:38 -0400

    Update compute.tf

--- Terraform/compute.tf
+++ Terraform/compute.tf
@@ -32,7 +32,11 @@ resource "null_resource" "copy_file_locally" {
   depends_on = [local_file.private_key_pem]
 
   provisioner "local-exec" {
-    command = "cp my_key.pem ~/.ssh/id_rsa && chmod 600 ~/.ssh/id_rsa"
+    command = "cp my_key.pem key && chmod 600 key"
+`   interpreter = ["bash", "-c"]
+    environment = {
+      "DEBUG" = "true"
+    }
   }
 }
 
