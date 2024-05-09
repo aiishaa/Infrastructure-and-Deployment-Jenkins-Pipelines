fdd446de8304c412ffafe6f087d3f607440db8e5
‪Aisha Fathy‬‏ <57088227+aiishaa@users.noreply.github.com>
2024-05-09 12:44:56 +0300

    Delete Terraform directory

--- Terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "s3-terraform-backend-bucket-1234"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "dynamo-state-lock-table"
-  }
-}
