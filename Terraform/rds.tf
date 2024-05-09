1a89d560d7fc35a727b74b5e43832b66f7d5c80a
‪Aisha Fathy‬‏ <57088227+aiishaa@users.noreply.github.com>
2024-05-09 14:32:40 +0300

    Update rds.tf

--- Terraform/rds.tf
+++ Terraform/rds.tf
@@ -11,8 +11,8 @@ resource "aws_db_subnet_group" "my_db_subnet_group" {
 resource "aws_security_group" "rds-SG" {
     vpc_id = module.my-vpc.my_vpc.id
     ingress {
-        from_port   = 3306
-        to_port     = 3306
+        from_port   = var.db_port
+        to_port     = var.db_port
         protocol    = "tcp"
         security_groups = [aws_security_group.backend_SG.id]
     }
