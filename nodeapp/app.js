5f7413721804e7d03c0df2742e017e27bae70e9d
‪Aisha Fathy‬‏ <57088227+aiishaa@users.noreply.github.com>
2024-05-09 22:55:30 +0300

    Update app.js

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -23,29 +23,29 @@ connection.connect(function(err) {
 connection.end();
 });})
 
-const redis = require('redis');
-const client = redis.createClient({
-    host: process.env.REDIS_HOSTNAME,
-    port: process.env.REDIS_PORT,
-});
+// const redis = require('redis');
+// const client = redis.createClient({
+//     host: process.env.REDIS_HOSTNAME,
+//     port: process.env.REDIS_PORT,
+// });
 
-client.on('error', err => {
-    console.log('Error ' + err);
-});
+// client.on('error', err => {
+//     console.log('Error ' + err);
+// });
 
-app.get('/redis', (req, res) => {
+// app.get('/redis', (req, res) => {
 
-  client.set('foo','bar', (error, rep)=> {                
-    if(error){     
-console.log(error);
-      res.send("redis connection failed");                             
-      return;                
-  }                 
-  if(rep){                          //JSON objects need to be parsed after reading from redis, since it is stringified before being stored into cache                      
- console.log(rep);
-  res.send("redis is successfuly connected");                 
- }}) 
-  })
+//   client.set('foo','bar', (error, rep)=> {                
+//     if(error){     
+// console.log(error);
+//       res.send("redis connection failed");                             
+//       return;                
+//   }                 
+//   if(rep){                          //JSON objects need to be parsed after reading from redis, since it is stringified before being stored into cache                      
+//  console.log(rep);
+//   res.send("redis is successfuly connected");                 
+//  }}) 
+//   })
   
   app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
