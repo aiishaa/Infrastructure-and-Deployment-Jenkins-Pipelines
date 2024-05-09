06803afc9ba2171e3f2a702df6da83d8e522ea2f
‪Aisha Fathy‬‏ <57088227+aiishaa@users.noreply.github.com>
2024-05-09 22:16:14 +0300

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,11 +1,10 @@
 def buildImage(String imageName) {
-    withCredentials([file(credentialsId: 'docker_env_file', variable: 'ENV_FILE')]) {
-        sh "docker build -t $imageName --env-file $ENV_FILE ."
-    }
+    echo "Building the Docker image..."
+    sh "docker build -t $imageName ."
 }
 
 def dockerLogin() {
-    echo "Building the Docker image..."
+    echo "Login to dockerhub..."
     withCredentials([usernamePassword(credentialsId: 'docker-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh "echo $PASS | docker login -u $USER --password-stdin"
     }
@@ -18,7 +17,9 @@ def dockerPush(String imageName) {
 
 def dockerRunContainer(String imageName) {
     echo "Starting the Node.js app container..."
-    sh "docker run --name nodeapp -p 3000:3000 $imageName"
+    withCredentials([file(credentialsId: 'docker_env_file', variable: 'ENV_FILE')]) {
+        sh "docker run --name nodeapp --env-file $ENV_FILE -p 3000:3000 $imageName"
+    }
 }
 
 return this
