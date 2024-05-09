9d84772b5f17e84ae0d1db672a4d1584eef191a6
‪Aisha Fathy‬‏ <57088227+aiishaa@users.noreply.github.com>
2024-05-09 19:45:16 +0300

    Update rds.tf

--- Terraform/rds.tf
+++ Terraform/rds.tf
@@ -11,20 +11,20 @@ resource "aws_db_subnet_group" "my_db_subnet_group" {
 resource "aws_security_group" "rds-SG" {
     vpc_id = module.my-vpc.my_vpc.id
     ingress {
-        from_port   = var.database_server['db_port']
-        to_port     = var.database_server['db_port']
+        from_port   = var.database_server["db_port"]
+        to_port     = var.database_server["db_port"]
         protocol    = "tcp"
         security_groups = [aws_security_group.backend_SG.id]
     }
 }
 
 resource "aws_db_instance" "rds-db" {
-  allocated_storage    = var.database_server['db_allocated_storage']
-  db_name              = var.database_server['db_name']
-  engine               = var.database_server['db_engine']
-  instance_class       = var.database_server['db_instance_class']
-  username             = var.database_server['db_username']
-  password             = var.database_server['db_password']
+  allocated_storage    = var.database_server["db_allocated_storage"]
+  db_name              = var.database_server["db_name"]
+  engine               = var.database_server["db_engine"]
+  instance_class       = var.database_server["db_instance_class"]
+  username             = var.database_server["db_username"]
+  password             = var.database_server["db_password"]
   skip_final_snapshot  = true
   vpc_security_group_ids = [aws_security_group.rds-SG.id]
   db_subnet_group_name = aws_db_subnet_group.my_db_subnet_group.name
